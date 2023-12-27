5b7e5066ada1a1f1975fe4eb056339080707c144
Chabashvili <sandro.chabashvili.88@gmail.com>
2023-12-27 16:19:28 +0400

    Last Changes

--- app.js
+++ app.js
@@ -1,19 +1,104 @@
 // BOLD VISITED
 
-function toggleBold(clickedElement){
-    var filterItems = document.querySelectorAll(".filter-buttons p");
-
-    filterItems.forEach(function(item){
+function toggleBold(clickedElement, filter) {
+    var filterItems = document.querySelectorAll(".filter-buttons button");
+    filterItems.forEach(function (item) {
         item.classList.remove('bold');
     });
 
     clickedElement.classList.add('bold');
+    const filterArray = ['open', 'closed', 'allpositions', 'priority', 'importance', 'secondary'];
+    const filteredObj = {
+        open: [0, 2, 3, 7],
+        closed: [1, 5, 6, 8],
+        allpositions: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        priority: [0, 2],
+        importance: [3, 4, 7],
+        secondary: [9]
+    };
+    var filteredList = document.querySelectorAll("#filter-obj > div");
+    filteredList.forEach(function (filterItem) {
+        filterItem.style.display = 'none';
+    })
+    for (var i = 0; i < filterArray.length; i++) {
+        var selected = filterArray[i];
+        if (filteredObj.hasOwnProperty(selected)) {
+            if (selected == filter) {
+                for (var x = 0; x < filteredObj[selected].length; x++) {
+                    filteredList.forEach(function (filterItem, key) {
+                        if(filteredObj[selected][x] == key){
+                            filterItem.style.display = 'block';
+                        }
+                    })
+                }
+            }
+        }
+    }
 }
 
-function toggleNav(navbarclick){
+
+
+    // filterArray.forEach(function(name){
+    //     console.log(filteredObj[name]);
+    //     // filteredObj[name].forEach(function(index){
+    //     for(var name in filteredObj){
+    //         filteredList.forEach(function(element, key){
+    //             if(key == index){
+    //                 element.style.display = 'block';
+    //             }
+    //         })
+    //     }
+    // })
+
+
+    // if(filter == 'allpositions'){
+    //     filteredList.forEach(function(filterItem){
+    //         filterItem.style.display = 'block';
+    //     })
+    // }
+    // if(filter == 'open'){
+    //     filteredList.forEach(function(filterItem, key){
+    //         if( key == 0 || key == 3 || key == 2 || key == 7){
+    //             filterItem.style.display = "block";
+    //         }
+    //     })
+    // }
+
+    // if(filter == 'closed'){
+    //     filteredList.forEach(function(filterItem, key){
+    //         if( key == 1 || key == 5 || key == 6 || key == 8){
+    //             filterItem.style.display = "block";
+    //         }
+    //     })
+    // }
+
+    // if(filter == 'priority'){
+    //     filteredList.forEach(function(filterItem, key){
+    //         if( key == 0 || key == 2){
+    //             filterItem.style.display = "block";
+    //         }
+    //     })
+    // }
+
+    // if(filter == 'importance'){
+    //     filteredList.forEach(function(filterItem, key){
+    //         if( key == 3 || key == 4 || key == 7){
+    //             filterItem.style.display = "block";
+    //         }
+    //     })
+    // }
+
+
+
+    //
+
+
+
+function toggleNav(navbarclick) {
+
     var filterNav = document.querySelectorAll(".nav-links li");
 
-    filterNav.forEach(function(item){
+    filterNav.forEach(function (item) {
         item.classList.remove('bold');
     });
 
@@ -22,104 +107,11 @@ function toggleNav(navbarclick){
 
 // LINK ROUTE
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     var defaultRoute = window.location.pathname.split('/')[1];
     var defaultItem = document.querySelector(".filter-buttons ." + defaultRoute);
 
-    if (defaultItem){
+    if (defaultItem) {
         toggleBold(defaultItem);
     }
 })
-
-
-// FILTER FUNCTIONAL MAIN
-
-var checkClass = function(){
-    if ($('.position').hasClass('hide')){
-        $('.position').removeClass('hide');
-    }
-};
-$('.allpositions').click(function(){
-    checkClass();
-});
-$('.open').click(function(){
-    $('.position:not(.open)').toggle('hide');
-});
-$('.priority').click(function(){
-    $('.position:not(.priority)').toggle('hide');
-});
-$('.importance').click(function(){
-    $('.position:not(.importance)').toggle('hide');
-});
-$('.secondary').click(function(){
-    $('.position:not(.secondary)').toggle('hide');
-});
-$('.closed').click(function(){
-    $('.position:not(.closed)').toggle('hide');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-// FILTER FUNCTIONAL
-
-// const filterButtons = document.querySelectorAll(".filter-buttons button");
-// const filterableCards = document.querySelectorAll(".filter-obj .position");
-
-// const filterCards = (e) => {
-//     document.querySelector(".filter-buttons .active").classList.remove("active");
-//     e.currentTarget.classList.add("active");
-
-//     const selectedFilter = e.currentTarget.querySelector('a').dataset.name;
-
-//     filterableCards.forEach((card) => {
-//         card.classList.add("hide");
-
-//         if (card.dataset.name === selectedFilter || selectedFilter === "all") {
-//             card.classList.remove("hide");
-//         }
-//     });
-// };
-
-// filterButtons.forEach((button) => button.addEventListener("click", filterCards));
-
-
-
-
-
-
-
-
-
-
-
-// ROUTE FOR NAVBAR --
-
-// document.addEventListener("DOMLoaded", function(){
-//     var defaultRoute2 = window.location.pathname.split('/')[1];
-//     var defaultItem2 = document.querySelector(".nav-links ." + defaultRoute2);
-
-//     if (defaultItem2){
-//         toggleBold(defaultItem2);
-//     }
-// })
-
-// TOGGLE FILTER UNDERLINE --
-
-// function toggleUnderline(clickUnderline){
-//     var underLine = document.querySelectorAll(".filter-buttons p");
-
-//     underLine.forEach(function(item){
-//         item.classList.remove('filter-buttons p::after');
-//     })
-
-//     clickUnderline.classList.add('filter-buttons p::after')
-// }
